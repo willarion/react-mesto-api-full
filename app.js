36274e862077b4ed325a17664eeac6ce682a4d3e
Marina Anisimova <anisimova.marina.n@gmail.com>
2021-01-25 18:52:24 +0300

    feature: реализовать валидацию запросов

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const { errors, celebrate, Joi } = require('celebrate');
 const bodyParser = require('body-parser');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
@@ -20,8 +21,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
+  }),
+}), createUser);
 
 app.use(auth);
 
@@ -31,6 +45,8 @@ app.use('/', cardsRouter);
 app.use('*', (req, res) => {
   res.status(404).send({ message: "Запрашиваемый ресурс не найден"});
 });
+
+app.use(errors()); //celebrate errors
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,29 @@
 const router = require('express').Router();
-const {getUsers, getUserProfile, getCurrentUserProfile, updateUserProfile, updateUserAvatar}= require('../controllers/users');
+const { celebrate, Joi } = require('celebrate');
+const { getUsers, getUserProfile, getCurrentUserProfile, updateUserProfile, updateUserAvatar }= require('../controllers/users');
 
 
 router.get('/users', getUsers);
 router.get('/users/me', getCurrentUserProfile);
-router.get('/users/:id', getUserProfile);
-router.patch('/users/me', updateUserProfile);
-router.patch('/users/me/avatar', updateUserAvatar);
+
+router.get('/users/:id', celebrate({
+  params: Joi.object().keys({
+    postId: Joi.string().alphanum().length(24),
+  }),
+}), getUserProfile);
+
+router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  })
+}), updateUserProfile);
+
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().uri(),
+  })
+}), updateUserAvatar);
 
 
 module.exports = router;
