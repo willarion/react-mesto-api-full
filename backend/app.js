bd11f97e986e24e91b834cdfb8ddf3266b7ee2b7
willarion <62561106+willarion@users.noreply.github.com>
2021-01-28 12:33:28 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,12 @@ app.use(requestLogger);
 
 app.use(cors());
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
